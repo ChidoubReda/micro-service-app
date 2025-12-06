ee867d037f455e83024593b4c6e1117e71658569
ChidoubReda <Reda.Chidoub@emsi-edu.ma>
2025-12-06 16:47:46 +0100

    Customer-Service

---
+++ customer-service/src/main/java/ma/emsi/chidoub/customerservice/repository/CustomerRepository.java
@@ -0,0 +1,9 @@
+package ma.emsi.chidoub.customerservice.repository;
+
+import ma.emsi.chidoub.customerservice.entities.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+}
